9f5046d8a43e4e340cd8e877ad654a3ddab45e86
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-03-26 22:23:33 +0100

    implement signin

--- src/app/logon/signin/signin.component.ts
+++ src/app/logon/signin/signin.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { SignInGuardService } from '../../auth/sign-in-guard.service';
+
+class SIgnInModel {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-signin',
@@ -7,10 +16,20 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class SigninComponent implements OnInit {
+  model: SIgnInModel;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {
+    this.model = new SIgnInModel();
+  }
 
   ngOnInit() {
   }
 
+  signIn() {
+    this.http.post(environment.serverUrl + '/users/signIn', this.model)
+    .subscribe(data => {
+      console.log(data);
+      this.router.navigate(['/dashboard']);
+    });
+  }
 }
