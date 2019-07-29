5d36f1d7f8a4ae386df478a97acd3091f47d95de
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-07-29 23:25:45 +0100

    Upgrade to Angular 8 (old code has been dropped because the UI is going to change)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,16 @@
-import { SigninComponent } from './logon/signin/signin.component';
-import { LogonModule } from './logon/logon.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './logon/register/register.component';
-import { ConfirmComponent } from './logon/confirm/confirm.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MatButtonModule } from '@angular/material';
-import { AuthModule } from './auth/auth.module';
-import { SignInGuardService as SignInGuard } from './auth/sign-in-guard.service';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/logon'
-  },
-  {
-    path: 'logon',
-    component: SigninComponent
-  },
-  {
-    path: 'logon/register',
-    component: RegisterComponent
-  },
-  {
-    path: 'logon/confirm',
-    component: ConfirmComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [SignInGuard]
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent
-  }
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes),
-    LogonModule,
-    DashboardModule,
-    AuthModule,
-    MatButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
