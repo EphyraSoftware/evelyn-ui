20354e5b619e83efb39a0c6bd9647e1df5d59524
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-11-15 00:44:50 +0000

    switch to secured service url

--- src/environments/environment.edge.ts
+++ src/environments/environment.edge.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serviceUrl: 'http://service.evelyn.internal:30021',
+  serviceUrl: 'https://service.evelyn.internal:30021',
   selfUrl: 'https://app.evelyn.internal:4200',
   keycloakConfig: {
     url: 'https://keycloak.evelyn.internal:30562/auth/',
