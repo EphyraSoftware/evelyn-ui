67c216eb135c089f60a2855833a545802fe9eb9d
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-01-15 00:25:13 +0000

    Display current month in preparation for calendar display work

--- src/app/calendar/calendar-management/calendar-management.component.ts
+++ src/app/calendar/calendar-management/calendar-management.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CalendarService} from '../calendar.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-calendar-management',
@@ -8,11 +9,45 @@ import {CalendarService} from '../calendar.service';
 })
 export class CalendarManagementComponent implements OnInit {
   private events$;
+  private cellValues: any[];
 
-  constructor(private calendarService: CalendarService) { }
+  constructor(private calendarService: CalendarService) {
+    this.generateDays();
+  }
 
   ngOnInit() {
     this.events$ = this.calendarService.getEvents();
   }
 
+  generateDays() {
+    const today = moment();
+
+    console.log('day', today.day());
+    const daysInMonth = today.daysInMonth();
+    console.log('days in month', daysInMonth);
+    const firstDayOfMonth = today.clone().date(1).day();
+    console.log('first day of month', firstDayOfMonth);
+
+    let cellCount = (firstDayOfMonth - 1) + daysInMonth;
+    cellCount += 7 - cellCount % 7;
+
+    console.log(cellCount);
+
+    const cellValues = [];
+    for (let i = 0; i < cellCount; i++) {
+      if (i % 7 === 0) {
+        cellValues.push([]);
+      }
+
+      if (i < firstDayOfMonth - 1) {
+        cellValues[Math.floor(i / 7)].push('-');
+      } else if (i < (firstDayOfMonth - 1) + daysInMonth) {
+        cellValues[Math.floor(i / 7)].push(i - (firstDayOfMonth - 1) + 1);
+      } else {
+        cellValues[Math.floor(i / 7)].push('-');
+      }
+    }
+
+    this.cellValues = cellValues;
+  }
 }
