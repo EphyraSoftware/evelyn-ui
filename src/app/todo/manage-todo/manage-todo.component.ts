7906da73f03cf0e5fb36ce89866afefbac79c5f3
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-02-09 01:56:06 +0000

    mute and strike through completed items

--- src/app/todo/manage-todo/manage-todo.component.ts
+++ src/app/todo/manage-todo/manage-todo.component.ts
@@ -16,6 +16,7 @@ export class ManageTodoComponent implements OnInit {
   stateIcon: IconDefinition;
 
   checkedValues: boolean[];
+  itemTextClasses: string[][];
 
   constructor(private todoService: TodoService, private formBuilder: FormBuilder) {
     this.open = false;
@@ -25,6 +26,16 @@ export class ManageTodoComponent implements OnInit {
   ngOnInit() {
     this.checkedValues = this.todo.items.map(value => {
       return value.complete;
+    });
+
+    this.itemTextClasses = this.checkedValues.map(value => {
+      const classes = ['ml-3'];
+      if (value) {
+        classes.push('text-muted');
+        classes.push('complete');
+      }
+
+      return classes;
     });
   }
 
@@ -41,7 +52,12 @@ export class ManageTodoComponent implements OnInit {
   toggleItemComplete(i: number) {
     this.checkedValues[i] = !this.checkedValues[i];
 
-    this.checkedValues.forEach(value => console.log(value));
+    if (this.checkedValues[i]) {
+      this.itemTextClasses[i].push('text-muted');
+      this.itemTextClasses[i].push('complete');
+    } else {
+      this.itemTextClasses[i] = this.itemTextClasses[i].filter(value => value !== 'text-muted' && value !== 'complete');
+    }
 
     const item = {
       text: this.todo.items[i].text,
